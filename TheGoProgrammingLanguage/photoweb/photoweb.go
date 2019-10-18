b5289a361adb5165efe1b75c03889f726d366973
qiangyin <qiangyin@sohu-inc.com>
2019-10-18 14:45:30 +0800

    html

--- TheGoProgrammingLanguage/photoweb/photoweb.go
+++ TheGoProgrammingLanguage/photoweb/photoweb.go
@@ -1,3 +1,10 @@
+/*
+ * @Description: In User Settings Edit
+ * @Author: your name
+ * @Date: 2019-10-18 14:21:25
+ * @LastEditTime: 2019-10-18 14:21:25
+ * @LastEditors: your name
+ */
 package main
 
 import (
@@ -13,11 +20,11 @@ const (
 
 func UploadHandler(w http.ResponseWriter, r *http.Request) {
 	if r.Method == "GET" {
-		io.WriteString(w, "<form method = \"POST\" action = \"upload\" "+
+		io.WriteString(w, "<html><form method = \"POST\" action = \"upload\" "+
 			" enctype = \"multipart/form-data\">"+
 			"Choose an image to upload: <input name = \"image\" type = \"file\" />"+
 			"<input type = \"submit\" value = \"upload\" />"+
-			"</form>")
+			"</form></html>")
 		return
 	} else if r.Method == "POST" {
 		f, h, err := r.FormFile("image")
