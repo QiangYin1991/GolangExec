6baa9c602a050b665c86674bad20064a252ebbb1
QiangYin1991 <qiangyin1991@gmail.com>
2019-10-20 22:17:33 +0800

    完结

--- TheGoProgrammingLanguage/photoweb/photoweb.go
+++ TheGoProgrammingLanguage/photoweb/photoweb.go
@@ -18,6 +18,7 @@ import (
 )
 
 const (
+	LIST_DIR     = 0x0001
 	UPLOAD_DIR   = "./uploads"
 	TEMPLATE_DIR = "./views"
 )
@@ -26,9 +27,7 @@ var templates = make(map[string]*template.Template)
 
 func init() {
 	fileInfoArr, err := ioutil.ReadDir(TEMPLATE_DIR)
-	if err != nil {
-		panic(err)
-	}
+	check(err)
 	var templateName, templatePath string
 	for _, fileInfo := range fileInfoArr {
 		templateName = fileInfo.Name()
@@ -51,22 +50,14 @@ func uploadHandler(w http.ResponseWriter, r *http.Request) {
 		return
 	} else if r.Method == "POST" {
 		f, h, err := r.FormFile("image")
-		if err != nil {
-			http.Error(w, err.Error(), http.StatusInternalServerError)
-			return
-		}
+		check(err)
 		filename := h.Filename
 		defer f.Close()
 		t, err := os.Create(UPLOAD_DIR + "/" + filename)
-		if err != nil {
-			http.Error(w, err.Error(), http.StatusInternalServerError)
-			return
-		}
+		check(err)
 		defer t.Close()
-		if _, err := io.Copy(t, f); err != nil {
-			http.Error(w, err.Error(), http.StatusInternalServerError)
-			return
-		}
+		_, err = io.Copy(t, f)
+		check(err)
 		http.Redirect(w, r, "/view?id="+filename, http.StatusFound)
 	}
 }
@@ -92,10 +83,7 @@ func isExists(path string) bool {
 
 func listHandler(w http.ResponseWriter, r *http.Request) {
 	fileInfoArr, err := ioutil.ReadDir(UPLOAD_DIR)
-	if err != nil {
-		http.Error(w, err.Error(), http.StatusInternalServerError)
-		return
-	}
+	check(err)
 	locals := make(map[string]interface{})
 	images := []string{}
 	for _, fileInfo := range fileInfoArr {
@@ -108,15 +96,47 @@ func listHandler(w http.ResponseWriter, r *http.Request) {
 	}
 }
 
+func staticDirHandler(mux *http.ServeMux, prefix string, staticDir string, flags int) {
+	mux.HandleFunc(prefix, func(w http.ResponseWriter, r *http.Request) {
+		file := staticDir + r.URL.Path[len(prefix)-1:]
+		if flags&LIST_DIR == 0 {
+			if exists := isExists(file); !exists {
+				http.NotFound(w, r)
+				return
+			}
+		}
+		http.ServeFile(w, r, file)
+	})
+}
+
 func renderHtml(w http.ResponseWriter, tmpl string, locals map[string]interface{}) error {
 	err := templates[tmpl].Execute(w, locals)
 	return err
 }
 
+func check(err error) {
+	if err != nil {
+		panic(err)
+	}
+}
+
+func safeHandler(fn http.HandlerFunc) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		defer func() {
+			if err, ok := recover().(error); ok {
+				http.Error(w, err.Error(), http.StatusInternalServerError)
+			}
+		}()
+		fn(w, r)
+	}
+}
+
 func main() {
-	http.HandleFunc("/", listHandler)
-	http.HandleFunc("/view", viewHandler)
-	http.HandleFunc("/upload", uploadHandler)
+	mux := http.NewServeMux()
+	staticDirHandler(mux, "/assets", "./public", 0)
+	http.HandleFunc("/", safeHandler(listHandler))
+	http.HandleFunc("/view", safeHandler(viewHandler))
+	http.HandleFunc("/upload", safeHandler(uploadHandler))
 	err := http.ListenAndServe(":8080", nil)
 	if err != nil {
 		log.Fatal("ListenAndServe: ", err.Error())
