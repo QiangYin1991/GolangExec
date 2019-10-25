23dc5033d9ddb1d8929d11d0031b088e801cdd12
qiangyin <qiangyin@sohu-inc.com>
2019-10-25 16:44:25 +0800

    rss.go

--- GoInAction/sample/matchers/rss.go
+++ GoInAction/sample/matchers/rss.go
@@ -1,6 +1,14 @@
 package matchers
 
-import "encoding/xml"
+import (
+	"GoInAction/sample/search"
+	"encoding/xml"
+	"errors"
+	"fmt"
+	"log"
+	"net/http"
+	"regexp"
+)
 
 type (
 	item struct {
@@ -21,11 +29,85 @@ type (
 	}
 
 	channel struct {
-		XMLName     xml.Name `xml:"name"`
-		Title       string   `xml:"title"`
-		Description string   `xml:"description"`
-		Link        string   `xml:"link"`
-		PubDate     string   `xml:"pubDate"`
-		LastB
+		XMLName        xml.Name `xml:"name"`
+		Title          string   `xml:"title"`
+		Description    string   `xml:"description"`
+		Link           string   `xml:"link"`
+		PubDate        string   `xml:"pubDate"`
+		LastBuildDate  string   `xml:"lastBuildDate"`
+		TTL            string   `xml:"ttl"`
+		Language       string   `xml:"language"`
+		ManagingEditor string   `xml:"managingEditor"`
+		WebMaster      string   `xml:"webMaster"`
+		Image          string   `xml:"image"`
+		Item           string   `xml:"item"`
+	}
+
+	rssDocument struct {
+		XMLName xml.Name `xml:"rss"`
+		Channel channel  `xml:"channel"`
 	}
 )
+
+type rssMatcher struct{}
+
+func init() {
+	var matcher rssMatcher
+	search.Register("rss", matcher)
+}
+
+func (m rssMatcher) retrieve(feed *search.Feed) (*rssDocument, error) {
+	if feed.URI == "" {
+		return nil, errors.New("No rss feed URI provided")
+	}
+
+	resp, err := http.Get(feed.URI)
+	if err != nil {
+		return nil, err
+	}
+	defer resp.Body.Close()
+
+	if resp.StatusCode != 200 {
+		return nil, fmt.Errorf("HTTP Response Error %d\n", resp.StatusCode)
+	}
+
+	var document rssDocument
+	err = xml.NewDecoder(resp.Body).Decode(&document)
+	return &document, err
+}
+
+func (m rssMatcher) Search(feed *search.Feed, searchTerm string) ([]*search.Result, error) {
+	var results []*search.Result
+	log.Printf("Search Feed Type[%s] Site[%s] For Uri[%s]\n", feed.Type, feed.Name, feed.URI)
+
+	document, err := m.retrieve(feed)
+	if err != nil {
+		return nil, err
+	}
+
+	for _, channelItem := range document.Channel.Item {
+		matched, err := regexp.MatchString(searchTerm, channelItem.Title)
+		if err != nil {
+			return nil, err
+		}
+		if matched {
+			results = append(results, &search.Result{
+				Field:   "Title",
+				Content: channelItem.Title,
+			})
+		}
+
+		matched, err := regexp.MatchString(searchTerm, channelItem.Description)
+		if err != nil {
+			return nil, err
+		}
+		if matched {
+			results = append(results, &search.Result{
+				Field:   "Description",
+				Content: channelItem.Description,
+			})
+		}
+	}
+
+	return results, nil
+}
