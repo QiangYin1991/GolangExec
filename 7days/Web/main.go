4398ec7bfba6134a1297348feb123c7757179b75
qiangyin <qiangyin@sohu-inc.com>
2020-03-26 17:37:43 +0800

    trie.go

--- 7days/Web/main.go
+++ 7days/Web/main.go
@@ -11,8 +11,8 @@ func main() {
 		context.HTML(http.StatusOK, "<h1>Hello Gee</h1>")
 	})
 
-	r.GET("/hello", func(context *gee.Context) {
-		context.String(http.StatusOK, "hello %s, you're at %s\n", context.Query("name"), context.Path)
+	r.GET("/hello/:name", func(context *gee.Context) {
+		context.String(http.StatusOK, "hello %s, you're at %s\n", context.Param("name"), context.Path)
 	})
 
 	r.POST("/login", func(context *gee.Context) {
@@ -20,6 +20,10 @@ func main() {
 			"username": context.PostForm("username"),
 			"password": context.PostForm("password"),
 		})
+	})
+
+	r.GET("/assets/*filepath", func(c *gee.Context) {
+		c.JSON(http.StatusOK, gee.H{"filepath": c.Param("filepath")})
 	})
 
 	r.Run(":9999")
